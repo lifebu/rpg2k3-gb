5be804a2ce5240a88c238f2107a05eddea7616de
Christian <chbur1@gmx.de>
2022-04-13 17:43:59 +0200

    Simple Project Generation Setup for Mapfiles.

--- src/tools/gb2rpg/src/ProjectGenerator.cpp
+++ src/tools/gb2rpg/src/ProjectGenerator.cpp
@@ -4,8 +4,12 @@
 #include "MapTree.h"
 #include "Database.h"
 
+#include <string>
+
 void genProjectFolder(std::vector<Map>& maps, MapTree& mapTree, Database& database) {
-    for(auto& map : maps) {
-        map.exportXML("");
+    for(int i = 0; i < maps.size(); ++i) {
+        // TODO: For more than 9 gb files this needs to be better, but is good enough for now!
+        std::string mapID = std::string("000") + std::to_string(i + 1);
+        maps.at(i).exportXML("playground/Map" + mapID + ".emu");
     }
 }
