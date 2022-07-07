dd4d4e87d5e1a8b3673400bc978457edd580c164
Christian <chbur1@gmx.de>
2022-07-07 20:41:37 +0200

    Rearranged folder structure to make it easier.

--- src/tools/gb2rpg/src/Globals.h
+++ src/tools/gb2rpg/src/Globals.h
@@ -4,39 +4,39 @@
 namespace TEMPLATES {
     // TODO: Can I do static_asserts to ensure that the files exist?
     // database
-    const static char* CHARACTER = "rpg2k3/templates/database/character.xml";
-    const static char* COMMON_EVENT = "rpg2k3/templates/database/common_event.xml";
-    const static char* DATABASE = "rpg2k3/templates/database/database.xml";
-    const static char* ITEM = "rpg2k3/templates/database/item.xml";
-    const static char* SWITCH = "rpg2k3/templates/database/switch.xml";
-    const static char* VARIABLE = "rpg2k3/templates/database/variable.xml";
+    const static char* CHARACTER = "templates/database/character.xml";
+    const static char* COMMON_EVENT = "templates/database/common_event.xml";
+    const static char* DATABASE = "templates/database/database.xml";
+    const static char* ITEM = "templates/database/item.xml";
+    const static char* SWITCH = "templates/database/switch.xml";
+    const static char* VARIABLE = "templates/database/variable.xml";
 
     // event
-    const static char* EVENT_COMMAND = "rpg2k3/templates/event/event_command.xml";
-    const static char* EVENT_PAGE = "rpg2k3/templates/event/event_page.xml";
-    const static char* EVENT = "rpg2k3/templates/event/event.xml";
+    const static char* EVENT_COMMAND = "templates/event/event_command.xml";
+    const static char* EVENT_PAGE = "templates/event/event_page.xml";
+    const static char* EVENT = "templates/event/event.xml";
 
     // map
-    const static char* MAP = "rpg2k3/templates/map/map.xml";
+    const static char* MAP = "templates/map/map.xml";
 
     // map tree
-    const static char* MAP_TREE = "rpg2k3/templates/map/map_tree.xml";
-    const static char* MAP_INFO = "rpg2k3/templates/map/map_info.xml";
+    const static char* MAP_TREE = "templates/map/map_tree.xml";
+    const static char* MAP_INFO = "templates/map/map_info.xml";
 
     // general
-    const static char* DMG_ROM = "rpg2k3/templates/dmg_rom.xml";
-    const static char* MAP_ROM_HEADER = "rpg2k3/templates/map_rom_header.xml";
-    const static char* MAP_ROM_LABEL = "rpg2k3/templates/map_rom_label.xml";
+    const static char* DMG_ROM = "templates/dmg_rom.xml";
+    const static char* MAP_ROM_HEADER = "templates/map_rom_header.xml";
+    const static char* MAP_ROM_LABEL = "templates/map_rom_label.xml";
 };
 
 namespace EXPORTS {
     // Map
-    const static char* MAP_BASE = "rpg2k3/project/Map";
+    const static char* MAP_BASE = "project/Map";
     const static char* MAP_TYPE = ".emu";
 
     // Maptree
-    const static char* MAP_TREE = "rpg2k3/project/RPG_RT.emt";
+    const static char* MAP_TREE = "project/RPG_RT.emt";
 
     // Database
-    const static char* DATABASE = "rpg2k3/project/RPG_RT.edb";
+    const static char* DATABASE = "project/RPG_RT.edb";
 };
