782bd68befddc12990019132a454fdb349d7e4a3
Christian Burmeister <mail@cburmeister.me>
2023-04-29 20:40:19 +0200

    Implemented InputManager.

--- src/tools/cppRPGenv/manager/InputManager.cpp
+++ src/tools/cppRPGenv/manager/InputManager.cpp
@@ -3,6 +3,7 @@
 #include <SFML/Window.hpp>
 
 #include <iostream>
+#include <cassert>
 
 namespace rpgenv
 {
@@ -20,36 +21,9 @@ InputManager* InputManager::Get()
 
 bool InputManager::isKeyPressed(RPGMAKER::KeyCodes key)
 {
-    switch(key)
+    if(sf::Keyboard::isKeyPressed(ConvertKeyCodeToSFML(key)))
     {
-        case RPGMAKER::KeyCodes::DOWN:
-        {
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-            {
-                return true;
-            }
-        }
-        case RPGMAKER::KeyCodes::LEFT:
-        {
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-            {
-                return true;
-            }
-        }
-        case RPGMAKER::KeyCodes::RIGHT:
-        {
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-            {
-                return true;
-            }
-        }
-        case RPGMAKER::KeyCodes::UP:
-        {
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-            {
-                return true;
-            }
-        }
+        return true;
     }
 
     return false;
@@ -63,6 +37,81 @@ void InputManager::Init()
 void InputManager::Shutdown() 
 {
 
+}
+
+sf::Keyboard::Key InputManager::ConvertKeyCodeToSFML(RPGMAKER::KeyCodes key)
+{
+    switch(key)
+    {
+        case RPGMAKER::KeyCodes::DOWN:
+            return sf::Keyboard::Down;
+    
+        case RPGMAKER::KeyCodes::LEFT:
+            return sf::Keyboard::Left;
+
+        case RPGMAKER::KeyCodes::RIGHT:
+            return sf::Keyboard::Right;
+
+        case RPGMAKER::KeyCodes::UP:
+            return sf::Keyboard::Up;
+
+        case RPGMAKER::KeyCodes::SELECT:
+            return sf::Keyboard::Enter;
+
+        case RPGMAKER::KeyCodes::CANCEL:
+            return sf::Keyboard::Escape;
+
+        case RPGMAKER::KeyCodes::SHIFT:
+            return sf::Keyboard::LShift;
+
+        case RPGMAKER::KeyCodes::NUMBER_0:
+            return sf::Keyboard::Num0;
+
+        case RPGMAKER::KeyCodes::NUMBER_1:
+            return sf::Keyboard::Num1;
+ 
+        case RPGMAKER::KeyCodes::NUMBER_2:
+            return sf::Keyboard::Num2;
+
+        case RPGMAKER::KeyCodes::NUMBER_3:
+            return sf::Keyboard::Num3;
+
+        case RPGMAKER::KeyCodes::NUMBER_4:
+            return sf::Keyboard::Num4;
+
+        case RPGMAKER::KeyCodes::NUMBER_5:
+            return sf::Keyboard::Num5;
+
+        case RPGMAKER::KeyCodes::NUMBER_6:
+            return sf::Keyboard::Num6;
+
+        case RPGMAKER::KeyCodes::NUMBER_7:
+            return sf::Keyboard::Num7;
+
+        case RPGMAKER::KeyCodes::NUMBER_8:
+            return sf::Keyboard::Num8;
+
+        case RPGMAKER::KeyCodes::NUMBER_9:
+            return sf::Keyboard::Num9;
+
+        case RPGMAKER::KeyCodes::SYMBOL_PLUS:
+            return sf::Keyboard::Add;
+
+        case RPGMAKER::KeyCodes::SYMBOL_MINUS:
+            return sf::Keyboard::Subtract;
+
+        case RPGMAKER::KeyCodes::SYMBOL_MUL:
+            return sf::Keyboard::Multiply;
+
+        case RPGMAKER::KeyCodes::SYMBOL_DIV:
+            return sf::Keyboard::Divide;
+
+        case RPGMAKER::KeyCodes::SYMBOL_DOT:
+            return sf::Keyboard::Comma;
+        
+        default:
+            return sf::Keyboard::Unknown;
+    }
 }
 
 };
--- src/tools/cppRPGenv/manager/InputManager.h
+++ src/tools/cppRPGenv/manager/InputManager.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <SFML/Window.hpp>
+
 #include "core/def/RPGMaker.h"
 
 namespace rpgenv 
@@ -17,6 +19,9 @@ public:
 
     void Init();
     void Shutdown();
+
+private:
+    sf::Keyboard::Key ConvertKeyCodeToSFML(RPGMAKER::KeyCodes key);
 };
 
 }
